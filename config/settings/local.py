23902340c824ad2e3345c0bf569f7d88431dfdb9
sanspie <sanspie@akarpov.ru>
2025-11-01 17:36:53 +0300

    fixed ci

--- config/settings/local.py
+++ config/settings/local.py
@@ -21,7 +21,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
-if env("USE_DOCKER") == "yes":
+if env("USE_DOCKER", default="no") == "yes":
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
